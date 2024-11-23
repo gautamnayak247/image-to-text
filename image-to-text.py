012819e3d0b06d6a50fdf389ee86b60ba36d4419
Gautam Nayak <gautamnayak247@hotmail.com>
2024-11-23 19:34:10 +0530

    minor update

--- image-to-text.py
+++ image-to-text.py
@@ -2,8 +2,9 @@ from dotenv import load_dotenv, find_dotenv
 from transformers import pipeline #download hugging face model to local machine
 # from langchain import PromptTemplate, LLMChain, OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+# from langchain.chains import LLMChain
 from langchain_openai  import ChatOpenAI
+# from langchain.schema import RunnableSequence
 import os
 
 load_dotenv(find_dotenv())
@@ -17,23 +18,26 @@ def generate_context(url:str) -> str:
         return "No context found"
 
 def generate_story_from_context(context: str) -> str:
-    template = """ 
-    You are a story teller:
-    You can generate a short story based on a sample narrative, the story should be no more than 20 words;
+    story_prompt = PromptTemplate.from_template("""
+                        You are a storyteller:
+                        You can generate a short story based on a sample narrative. 
+                        The story should be no more than 20 words.
 
-    CONTEXT: {context}
-    STORY:
-    """
-    
-    prompt = PromptTemplate(template = template, input_variables = ["context"])
-    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature =1, openai_api_key=os.getenv("OPENAI_API_KEY"))
+                        CONTEXT: {context}
+                        STORY:
+                    """)
+    llm = ChatOpenAI(model="gpt-3.5-turbo", 
+                     temperature =1, 
+                     openai_api_key=os.getenv("OPENAI_API_KEY"))
+    story_pipeline = story_prompt | llm
 
-    story_llm  = LLMChain(llm= llm, prompt = prompt, verbose=True)
-    story = story_llm.run(context = context)
+    #story_llm  = LLMChain(llm= llm, prompt = prompt)
+    story = story_pipeline.invoke({"context": context})
     return story.strip()
 
 def main():
     context = generate_context(".\\image\\xmas.jpg")
+    print(context)
     story = generate_story_from_context(context)
     print(story)
     return
